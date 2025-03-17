a38893e5547d73e7d328235635082d196822e2dd
rishi0810 <rishi.1003raj@gmail.com>
2025-03-17 20:06:10 +0530

    AAAAAA

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bable.vercel.app');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(
   cors({
     origin: 'https://bable.vercel.app',

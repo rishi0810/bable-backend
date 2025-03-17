6d9ac00e6609f2fde164f6f331f25438569b6d7e
rishi0810 <rishi.1003raj@gmail.com>
2025-03-17 19:48:02 +0530

    Even more issues

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import userroute from "./routes/user.js";
 import blogroute from "./routes/blog.js";
 import connectDB from "./db/blog.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,7 +14,15 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, origin || "*");
+    },
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/user", userroute);
 app.use("/blog", blogroute);
 

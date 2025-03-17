92f7402a4f7008625d467746920aabd567a49b7c
rishi0810 <rishi.1003raj@gmail.com>
2025-03-17 20:11:15 +0530

    Close to Giving up

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Set-Cookie');
   next();
 });
 app.use(

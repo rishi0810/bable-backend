915398551db4e9d1dfd4cc7b73e116beceade3eb
rishi0810 <rishi.1003raj@gmail.com>
2025-03-17 19:54:02 +0530

    Refuses to get fixed

--- index.js
+++ index.js
@@ -14,15 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      callback(null, origin || "*");
-    },
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use("/user", userroute);
 app.use("/blog", blogroute);
 

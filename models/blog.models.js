96808833eb963765260cc9a095171020331e106b
rishi0810 <rishi.1003raj@gmail.com>
2025-06-15 20:36:30 +0530

    some additional functionalities

--- models/blog.models.js
+++ models/blog.models.js
@@ -23,7 +23,11 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-});
+}, 
+{
+  timestamps : true
+}
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 export default Blog;
